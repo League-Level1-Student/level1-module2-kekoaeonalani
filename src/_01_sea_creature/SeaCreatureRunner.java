3b28e3d6fd7426e8b125db38399744131141e931
league <asherwelsbie@icloud.com>
2023-11-16 19:54:58 -0800

    Continue sea creatures

---
+++ src/_01_sea_creature/SeaCreatureRunner.java
@@ -0,0 +1,9 @@
+package _01_sea_creature;
+
+public class SeaCreatureRunner {
+SeaCreature spongeBob = new SeaCreature("Sponge Bob");
+SeaCreature patrick = new SeaCreature("Patrick");
+SeaCreature squidward= new SeaCreature("Squidward");
+
+
+}
